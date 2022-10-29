af6e312d902e9aa24f84780aeda3801050ee7d04
Alexandr Sidorenkov <alexandr-sidorenkov@yandex.ru>
2022-10-29 20:50:29 +0400

    task-1, task-2 finished. Submit tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Separator from './Components/Separator/Separator';
-// import Time from './Components/Time/Time';
+import Time from './Components/Time/Time';
 import Highlight from './Components/Highlight/Highlight';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <h1 className=''>Курс React.<br></br>Домашнее задание по&nbsp;теме<br></br>"HOC - Higher Order Components"</h1>
       <h2 className='mx-auto text-center'>Задание 1. "Форматирование даты публикации (Times)"</h2>
       <div className='task-1' id='task-1'>
-        {/* <Time/> */}
+        <Time/>
       </div>
       <Separator/>
       <h2 className='mx-auto text-center'>Задание 2. "Подсвечивание блоков (Highlight)"</h2>
